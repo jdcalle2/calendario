42ea15c07c56832d4f143c6698063e566ecf9568
juanpablo-jpho <60911217+juanpablo-jpho@users.noreply.github.com>
2022-05-30 20:04:43 -0500

    cambio1

--- src/main/kotlin/com/example/academico/service/TeacherService.kt
+++ src/main/kotlin/com/example/academico/service/TeacherService.kt
@@ -8,40 +8,6 @@ import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.server.ResponseStatusException
-//I tried to kill the pain
-//But only brought more (so much more)
-//I lay dying
-//And I'm pouring crimson regret and betrayal
-//I'm dying, praying, bleeding and screaming
-//Am I too lost to be saved?
-//Am I too lost?
-//My God, my Tourniquet
-//Return to me salvation
-//My God, my Tourniquet
-//Return to me salvation
-//Do you remember me?
-//Lost for so long
-//Will you be on the other side?
-//Or will you forget me?
-//I'm dying, praying, bleeding and screaming
-//Am I too lost to be saved?
-//Am I too lost?
-//My God, my Tourniquet
-//Return to me salvation
-//My God, my Tourniquet
-//Return to me salvation
-//I want to die
-//My God, my Tourniquet
-//Return to me salvation
-//My God, my Tourniquet
-//Return to me salvation
-//My wounds cry for the grave
-//My soul cries for deliverance
-//Will I be denied Christ?
-//Tourniquet
-//My suicide
-//Return to me salvation
-//Return to me salvation
 
 @Service
 class TeacherService {
