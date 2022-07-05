8cdea144550398704fdae9585887f6d3e523d75a
juanpablo-jpho <60911217+juanpablo-jpho@users.noreply.github.com>
2022-07-04 19:58:06 -0500

    trabajo

--- src/main/kotlin/com/example/academico/service/StudentService.kt
+++ src/main/kotlin/com/example/academico/service/StudentService.kt
@@ -102,4 +102,28 @@ class StudentService {
         return "${rowsUpdate} rows updated"
     }
 
+    fun validateSerialNumber (serial:String?): Boolean?{
+
+        serial.takeIf { !it?.trim().isNullOrEmpty() }
+            ?: throw Exception()
+
+        if (serial?.substring(0, 3).equals("786"))
+            return true
+
+        return false
+
+
+
+        /*try {
+                serial.takeIf { !it?.trim().isNullOrEmpty() }
+                    ?: throw Exception()
+                if (serial?.substring(0, 3).equals("786"))
+                    return true
+                return false
+            }
+            catch (e: Exception) {
+                throw Exception()
+            }*/
+    }
+
 }
---
+++ src/test/kotlin/service/OpticabeAplicationTests.kt
@@ -0,0 +1,10 @@
+package service
+
+import org.springframework.boot.test.context.SpringBootTest
+@SpringBootTest
+
+class OpticabeAplicationTests {
+
+
+
+}
