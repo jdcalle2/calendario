67c94f6a5a712aa1822d3eacbfc297bfe8804e96
juanpablo-jpho <60911217+juanpablo-jpho@users.noreply.github.com>
2022-07-11 20:15:51 -0500

    tarea23

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -47,3 +47,12 @@ CREATE TABLE IF NOT EXISTS futball (
 PRIMARY KEY (id)
 
 );
+
+
+CREATE TABLE IF NOT EXISTS pago (
+    id SERIAL,
+    placa VARCHAR (8) NOT NULL,
+    anio int not null,
+    valor int not null,
+    PRIMARY KEY (id)
+    );
